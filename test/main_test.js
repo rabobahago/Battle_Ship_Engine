e0ce1aa38f74190872d0ee4ae58292381d2a9ce7
rabobahago <rabobahago@gmail.com>
2020-10-29 09:16:37 +0100

    project files added

---
+++ test/main_test.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect
+// Test suite
+describe('mocha', () => {
+  //Test spec (unit test)
+  it('should run our test using npm', () => {
+    expect(true).to.be.ok
+  })
+})
